ed9b86a808dba0f06d37d3c92a351c00aba4169c
Joseph Enubiak <enubiakjoseph@gmail.com>
2023-06-06 12:47:19 +0100

    api gateway

---
+++ src/main/java/huncho/com/apigatewatservice/ApiGatewatServiceApplication.java
@@ -0,0 +1,13 @@
+package huncho.com.apigatewatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewatServiceApplication.class, args);
+	}
+
+}
